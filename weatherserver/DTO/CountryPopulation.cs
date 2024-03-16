a02e952d382808da2fbe41470de8e270f9e1bc64
makenana <makenabambrick17@gmail.com>
2024-03-15 20:48:10 -0700

    03/15/2024 added population, added controller, added DTO, added Data (csv)

---
+++ weatherserver/DTO/CountryPopulation.cs
@@ -0,0 +1,11 @@
+﻿namespace weatherserver.DTO
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set; }
+
+        public int CountryId { get; set; }
+
+        public int Population { get; set; }
+    }
+}
